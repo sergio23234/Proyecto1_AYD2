dfbd94d22b45e93c29235797b9edc91c71cdc2d0
Sergio De los Rios <sergiofernando23234@gmail.com>
2018-04-28 17:05:48 -0600

    subiendo
    aplicacion

---
+++ Proyecto1_ayd2/Proyecto1_ayd2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Proyecto1_ayd2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textcuenta = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textcontra = new System.Windows.Forms.TextBox();
+            this.entrar = new System.Windows.Forms.Button();
+            this.registro = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "No de cuenta";
+            // 
+            // textcuenta
+            // 
+            this.textcuenta.Location = new System.Drawing.Point(166, 152);
+            this.textcuenta.Name = "textcuenta";
+            this.textcuenta.Size = new System.Drawing.Size(141, 20);
+            this.textcuenta.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(153, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textcontra
+            // 
+            this.textcontra.Location = new System.Drawing.Point(166, 234);
+            this.textcontra.Name = "textcontra";
+            this.textcontra.Size = new System.Drawing.Size(141, 20);
+            this.textcontra.TabIndex = 3;
+            // 
+            // entrar
+            // 
+            this.entrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.entrar.Location = new System.Drawing.Point(193, 298);
+            this.entrar.Margin = new System.Windows.Forms.Padding(2);
+            this.entrar.Name = "entrar";
+            this.entrar.Size = new System.Drawing.Size(75, 23);
+            this.entrar.TabIndex = 4;
+            this.entrar.Text = "Entrar";
+            this.entrar.UseVisualStyleBackColor = false;
+            // 
+            // registro
+            // 
+            this.registro.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.registro.Location = new System.Drawing.Point(272, 298);
+            this.registro.Margin = new System.Windows.Forms.Padding(2);
+            this.registro.Name = "registro";
+            this.registro.Size = new System.Drawing.Size(75, 23);
+            this.registro.TabIndex = 5;
+            this.registro.Text = "Registrar";
+            this.registro.UseVisualStyleBackColor = false;
+            this.registro.Click += new System.EventHandler(this.registro_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(459, 377);
+            this.Controls.Add(this.registro);
+            this.Controls.Add(this.entrar);
+            this.Controls.Add(this.textcontra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textcuenta);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textcuenta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textcontra;
+        private System.Windows.Forms.Button entrar;
+        private System.Windows.Forms.Button registro;
+    }
+}
+
