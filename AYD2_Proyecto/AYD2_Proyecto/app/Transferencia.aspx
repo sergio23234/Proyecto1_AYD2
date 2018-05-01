d49153b87c99d005632adea10249118503641935
Luis Garcia <luis13mzm@gmail.com>
2018-05-01 17:49:36 -0600

    Transferencia
    Pagina agregada, falta funcionalidad

---
+++ AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/app/MasterPage.master" AutoEventWireup="true" CodeFile="Transferencia.aspx.cs" Inherits="app_Transaccion" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <h1>Transferencia Monetaria</h1>
+    <br />
+
+    <asp:Label runat="server"><h3>Seleccione la cuenta destino:</h3></asp:Label>
+    <asp:DropDownList runat="server" ID="ddlist"></asp:DropDownList>
+    <br /><br />
+    
+    <asp:Button ID="bttrans" runat="server" Text="Aceptar" OnClick="bttrans_Click"/>
+    <br />
+
+
+</asp:Content>
+
---
+++ AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class app_Transaccion : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+        con.Open();
+        string query = "SELECT Cuenta, CONCAT(Cuenta, ' - ', Nombre, ' ', Apellido) AS 'Datos' FROM Usuario WHERE Cuenta != '" + Session["cuenta"] + "' ";
+        SqlCommand cmd = new SqlCommand(query, con);
+        ddlist.DataSource = cmd.ExecuteReader();
+        ddlist.DataTextField = "Datos";
+        ddlist.DataValueField = "Cuenta";
+        ddlist.DataBind();
+    }
+
+    protected void bttrans_Click(object sender, EventArgs e)
+    {
+
+    }
+}
