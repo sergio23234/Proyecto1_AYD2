1adeb62621362c7006e16153a18da4afd88de49b
Luis Garcia <luis13mzm@gmail.com>
2018-05-03 00:30:31 -0600

    Transferencia
    Transferencia entre cuentas

--- AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx
+++ AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx
@@ -3,8 +3,18 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
     <h1>Transferencia Monetaria</h1>
     <br />
+
+    <asp:Label><h3>Saldo Actual:</h3></asp:Label>
+    <p>Q. <%:Session["saldo"] %></p>
+    <br />
+
+    <asp:Label><h3>Monto a transferir:</h3></asp:Label>
+    <asp:TextBox ID="txtMonto" runat="server"></asp:TextBox>
+    <asp:RegularExpressionValidator ID="regex1" runat="server" ValidationExpression="((\d+)((\.\d{1,2})?))$" ErrorMessage="Debe ingresar un monto valido. Solo se permiten numeros con un max. de 2 decimales." ControlToValidate="txtMonto"></asp:RegularExpressionValidator>
+    <br /><br />
 
     <asp:Label runat="server"><h3>Seleccione la cuenta destino:</h3></asp:Label>
     <asp:DropDownList runat="server" ID="ddlist"></asp:DropDownList>
@@ -12,6 +22,7 @@
     
     <asp:Button ID="bttrans" runat="server" Text="Aceptar" OnClick="bttrans_Click"/>
     <br />
+    <asp:Label ID="lb_error" runat="server"><%:Session["error_transferencia"] %></asp:Label>
 
 
 </asp:Content>
--- AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx.cs
+++ AYD2_Proyecto/AYD2_Proyecto/app/Transferencia.aspx.cs
@@ -20,10 +20,45 @@ public partial class app_Transaccion : System.Web.UI.Page
         ddlist.DataTextField = "Datos";
         ddlist.DataValueField = "Cuenta";
         ddlist.DataBind();
+        con.Close();
+
+        if (!this.IsPostBack)
+        {
+            Session["error_transferencia"] = null;
+        }
     }
 
     protected void bttrans_Click(object sender, EventArgs e)
     {
+        double monto = Convert.ToDouble(txtMonto.Text);
+        string cuenta_destino = ddlist.SelectedValue.ToString();
 
+        System.Diagnostics.Debug.WriteLine(cuenta_destino + " - " + monto.ToString());
+
+        if (monto > Convert.ToDouble(Session["saldo"].ToString()))
+        {
+            Session["error_transferencia"] = "Error. No se cuenta con los fondos suficientes para realizar la transferencia.";            
+            //Response.Redirect("~/app/Transferencia.aspx");
+        }
+        else
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+            con.Open();
+            string query = "UPDATE Usuario SET saldo = saldo + " + monto + " WHERE Cuenta = '" + cuenta_destino + "' ";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.ExecuteScalar();
+
+            query = "UPDATE Usuario SET saldo = saldo - " + monto + " WHERE Cuenta = '" + Session["cuenta"] + "' ";
+            cmd = new SqlCommand(query, con);
+            cmd.ExecuteScalar();
+
+            Session["saldo"] = (Convert.ToDouble(Session["saldo"].ToString()) - monto).ToString();
+
+            Session["error_transferencia"] = "Transferencia realizada con exito.";
+            txtMonto.Text = "";
+            //Response.Redirect("~/app/Transferencia.aspx");
+
+            con.Close();
+        }
     }
 }
